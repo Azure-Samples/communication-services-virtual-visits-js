49f7a5e847d0cc4238dd136857d45e909ef69173
Aline Caillaud <alcail@microsoft.com>
2022-04-08 11:25:43 -0700

    fix re-fetching token when joinning meeting

--- client/src/Visit.tsx
+++ client/src/Visit.tsx
@@ -1,25 +1,27 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import React from 'react';
 import { CommunicationUserToken } from '@azure/communication-identity';
 import { LayerHost, Spinner, Stack, ThemeProvider } from '@fluentui/react';
+import React from 'react';
 import { Header } from './Header';
+import { GenericError } from './components/GenericError';
 import { MeetingExperience } from './components/MeetingExperience';
 import { JoinTeamsMeeting } from './components/JoinTeamsMeeting';
 import { AppConfigModel } from './models/ConfigModel';
 import { TeamsMeetingLinkModel } from './models/TeamsMeetingLinkModel';
+import './styles/Common.css';
+import { backgroundStyles, fullSizeStyles } from './styles/Common.styles';
 import { fetchConfig } from './utils/FetchConfig';
 import { fetchToken } from './utils/FetchToken';
 import { getTeamsMeetingLink } from './utils/GetTeamsMeetingLink';
-import { backgroundStyles, fullSizeStyles } from './styles/Common.styles';
-import './styles/Common.css';
-import { GenericError } from './components/GenericError';
+// import { getCurrentMeetingURL, getTeamsMeetingLink, makeJoinUrl } from './utils/GetTeamsMeetingLink';
 
 interface VisitState {
   config: AppConfigModel | undefined;
   token: CommunicationUserToken | undefined;
   error: any | undefined;
+  meetingLinkModel: TeamsMeetingLinkModel | undefined;
 }
 
 interface VisitProps {
@@ -27,13 +29,42 @@ interface VisitProps {
 }
 
 export class Visit extends React.Component<VisitProps, VisitState> {
+  private _getMeetingLinkModel(meetingLink: string): TeamsMeetingLinkModel | undefined {
+    let meetingLinkModel: TeamsMeetingLinkModel | undefined = undefined;
+
+    if (meetingLink) {
+      // try extracting Teams link from the url
+      try {
+        meetingLinkModel = getTeamsMeetingLink(meetingLink);
+      } catch (error) {
+        meetingLinkModel = undefined;
+      }
+    }
+
+    return meetingLinkModel;
+  }
+
+  private _onJoinMeeting(link: string): void {
+    window.history.pushState({}, document.title, window.location.href + link);
+    this.setState({ meetingLinkModel: this._getMeetingLinkModel(link) });
+  }
+
+  private _resetVisit(): void {
+    window.location.assign(window.location.href);
+  }
+
   public constructor(props: VisitProps) {
     super(props);
+
+    this._onJoinMeeting.bind(this);
+
+    window.onpopstate = () => this._resetVisit();
 
     this.state = {
       config: undefined,
       token: undefined,
-      error: undefined
+      error: undefined,
+      meetingLinkModel: this._getMeetingLinkModel(window.location.search) // case of direct link to visit with meeting link in URL
     };
   }
 
@@ -52,56 +83,49 @@ export class Visit extends React.Component<VisitProps, VisitState> {
     if (this.state.error) {
       return <GenericError statusCode={this.state.error.statusCode} />;
     } else {
-      let meetingLinkModel: TeamsMeetingLinkModel | undefined;
-
-      // try extracting Teams link from the url
-      try {
-        meetingLinkModel = getTeamsMeetingLink(window.location.search);
-      } catch (error) {
-        meetingLinkModel = undefined;
-      }
-
       // if the state is loaded and we got a valid teams link - try to start
       // the meeting experience
-      if (this.state.config && this.state.token && meetingLinkModel) {
-        const locator = { meetingLink: meetingLinkModel.meetingUrl };
-        const parentID = 'VisitSection';
+      if (this.state.config && this.state.token) {
+        if (this.state.meetingLinkModel) {
+          const locator = { meetingLink: this.state.meetingLinkModel.meetingUrl };
+          const parentID = 'VisitSection';
 
-        return (
-          <ThemeProvider theme={this.state.config.theme} style={{ height: '100%' }}>
-            <Stack styles={backgroundStyles(this.state.config.theme)}>
-              <Header companyName={this.state.config.companyName} parentid={parentID} />
-              <LayerHost
-                id={parentID}
-                style={{
-                  position: 'relative',
-                  height: '100%'
-                }}
-              >
-                <MeetingExperience
-                  userId={this.state.token.user}
-                  token={this.state.token.token}
-                  displayName="Virtual Visits User"
-                  endpointUrl={this.state.config.communicationEndpoint}
-                  locator={locator}
-                  fluentTheme={this.state.config.theme}
-                  logoUrl={this.state.config.logoUrl}
-                  waitingTitle={this.state.config.waitingTitle}
-                  waitingSubtitle={this.state.config.waitingSubtitle}
-                  chatEnabled={this.state.config.chatEnabled}
-                />
-              </LayerHost>
-            </Stack>
-          </ThemeProvider>
-        );
-      } else if (this.state.config && !meetingLinkModel) {
-        // If we have correct state but don't have a meeting link,
-        // show a separate screen with "enter meeting link" textbox
-        return (
-          <ThemeProvider theme={this.state.config.theme} style={{ height: '100%' }}>
-            <JoinTeamsMeeting config={this.state.config} />
-          </ThemeProvider>
-        );
+          return (
+            <ThemeProvider theme={this.state.config.theme} style={{ height: '100%' }}>
+              <Stack styles={backgroundStyles(this.state.config.theme)}>
+                <Header companyName={this.state.config.companyName} parentid={parentID} />
+                <LayerHost
+                  id={parentID}
+                  style={{
+                    position: 'relative',
+                    height: '100%'
+                  }}
+                >
+                  <MeetingExperience
+                    userId={this.state.token.user}
+                    token={this.state.token.token}
+                    displayName="Virtual Visits User"
+                    endpointUrl={this.state.config.communicationEndpoint}
+                    locator={locator}
+                    fluentTheme={this.state.config.theme}
+                    logoUrl={this.state.config.logoUrl}
+                    waitingTitle={this.state.config.waitingTitle}
+                    waitingSubtitle={this.state.config.waitingSubtitle}
+                    chatEnabled={this.state.config.chatEnabled}
+                  />
+                </LayerHost>
+              </Stack>
+            </ThemeProvider>
+          );
+        } else {
+          // If we have valid config and token but no meeting link,
+          // show a separate screen with "enter meeting link" textbox
+          return (
+            <ThemeProvider theme={this.state.config.theme} style={{ height: '100%' }}>
+              <JoinTeamsMeeting config={this.state.config} onJoinMeeting={(link) => this._onJoinMeeting(link)} />
+            </ThemeProvider>
+          );
+        }
       } else {
         // State not ready yet - show spinning/loading animation
         return <Spinner styles={fullSizeStyles} />;
