c5ae9293ce6eb4f7a40b659ca128e97d158ad02e
tejaswip-msft <tejaswiputhi@microsoft.com>
2022-04-13 23:41:12 +0530

    changes to catch 404 from iframe

--- client/src/Book.tsx
+++ client/src/Book.tsx
@@ -2,7 +2,7 @@
 // Licensed under the MIT license.
 
 import React from 'react';
-import { LayerHost, Spinner, Stack, ThemeProvider } from '@fluentui/react';
+import { LayerHost, Spinner, Stack, ThemeProvider, Link, Text } from '@fluentui/react';
 import { backgroundStyles, fullSizeStyles } from './styles/Common.styles';
 import { embededIframeStyles } from './styles/Book.styles';
 import { Header } from './Header';
@@ -40,23 +40,43 @@ export class Book extends React.Component<BookProps, BookState> {
     }
   }
 
+  public iframeGenerator(conditionalElement: JSX.Element, iframeContentUrl: string) {
+    const httpRequest = new XMLHttpRequest();
+    const text =
+      'The bookings page you are trying to reach is unavailable. The page may have been removed, deleted or the URL incorrectly entered';
+    httpRequest.onreadystatechange = function () {
+      if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+          // success
+          conditionalElement;
+        } else {
+          // failure. Act as you want
+          <Text block={true}>
+            {text}
+            <Link tabIndex={0} data-testid="bookingsSetupLink" target="_blank" href={iframeContentUrl}>
+              {'Learn More'}
+            </Link>
+          </Text>;
+        }
+      }
+    };
+    httpRequest.open('GET', iframeContentUrl);
+    httpRequest.send();
+  }
+
   render(): JSX.Element {
     const parentID = 'BookMeetingSection';
     if (this.state.config) {
+      const conditionalElement = (
+        <LayerHost id={parentID} style={{ position: 'relative', height: '100%' }}>
+          <iframe src={this.state.config.microsoftBookingsUrl} scrolling="yes" style={embededIframeStyles}></iframe>
+        </LayerHost>
+      );
       return (
         <ThemeProvider theme={this.state.config.theme} style={{ height: '100%' }}>
           <Stack styles={backgroundStyles(this.state.config.theme)}>
             <Header companyName={this.state.config.companyName} parentid={parentID} />
-            <LayerHost
-              id={parentID}
-              style={{
-                position: 'relative',
-                height: '100%',
-                overflow: 'hidden'
-              }}
-            >
-              <iframe src={this.state.config.microsoftBookingsUrl} scrolling="yes" style={embededIframeStyles}></iframe>
-            </LayerHost>
+            {this.iframeGenerator(conditionalElement, this.state.config.microsoftBookingsUrl)}
           </Stack>
         </ThemeProvider>
       );
