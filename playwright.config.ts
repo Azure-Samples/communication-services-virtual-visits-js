b25511afffc08941ef67ab6abbf34bf89fc13788
tejaswiputhi-msft <tejaswiputhi@microsoft.com>
2022-06-08 14:17:43 -0700

    config comment removal

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,10 +33,6 @@ const config: PlaywrightTestConfig = {
   // /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
 
-  /* Workers should be set to 1 to make sure test is run sequentially in approach 2 */
-
-  //workers: 1
-
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
