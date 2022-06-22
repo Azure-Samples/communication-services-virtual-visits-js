0753503f2385a83a62e990f002647d8ac72b0505
tejaswiputhi-msft <tejaswiputhi@microsoft.com>
2022-06-22 13:49:25 -0700

    simplyfying the get uuid function

--- e2e-tests/tests/common/utils.ts
+++ e2e-tests/tests/common/utils.ts
@@ -7,25 +7,10 @@ export const buildUrl = (serverUrl: string, page: string): string =>
 export const DELAY_MS = 1200;
 
 function getGUID() {
-  let timeStamp = new Date().getTime();
-  let timeInMilliSec =
-    (performance && performance.now && performance.now() * 1000) || 0;
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (
-    digit
-  ) {
-    let randomNumber = Math.random() * 16; //random number between 0 and 16
-    if (timeStamp > 0) {
-      //Use timestamp until depleted
-      randomNumber = (timeStamp + randomNumber) % 16 | 0;
-      timeStamp = Math.floor(timeStamp / 16);
-    } else {
-      //Use microseconds since page-load if supported
-      randomNumber = (timeInMilliSec + randomNumber) % 16 | 0;
-      timeInMilliSec = Math.floor(timeInMilliSec / 16);
-    }
-    return (digit === "x" ? randomNumber : (randomNumber & 0x3) | 0x8).toString(
-      16
-    );
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    let randomNumber = (Math.random() * 16) | 0;
+    let randomValue = c == "x" ? randomNumber : (randomNumber & 0x3) | 0x8;
+    return randomValue.toString(16);
   });
 }
 
