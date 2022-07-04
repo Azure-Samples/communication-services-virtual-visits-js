1577db86a6ce9a4c5aa281f795acab58d8ee9109
Han Kyung Sung <hansung@microsoft.com>
2022-07-04 11:41:01 -0700

    Set overlay errors to false to not show the error on overlay.

--- client/config/webpackDevServer.config.js
+++ client/config/webpackDevServer.config.js
@@ -81,7 +81,7 @@ module.exports = function (proxy, allowedHost) {
         port: sockPort
       },
       overlay: {
-        errors: true,
+        errors: false,
         warnings: false
       }
     },
