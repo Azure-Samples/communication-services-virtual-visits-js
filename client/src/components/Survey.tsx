0e6d8e0fdfacc765609b831d4dafea213815b742
tsohoni-msft <93625742+tsohoni-msft@users.noreply.github.com>
2022-09-22 09:45:59 -0700

    Config changes for post-call survey V2 (#91)
    * Adding config changes for 1-question poll
    
    * Add variables to ARM templates and default config
    
    * Fixing incorrectly merged test
    
    * Fixing test and bug in the code
    
    * Reverting changes for defaultConfig and ARMs
    
    * Readding ARM changes as discussed
    
    * Changes made after review comments
    
    * More review changes
    
    * Fix tests + fix review comments
    
    * Fixing missed lint error.
    
    * Adding missing stuff regarding answerPlaceholder

--- client/src/components/Survey.tsx
+++ client/src/components/Survey.tsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import { Link, Stack } from '@fluentui/react';
-import { PostCallConfig } from '../models/ConfigModel';
+import { CustomSurveyOptions, MSFormsSurveyOptions, PostCallConfig } from '../models/ConfigModel';
 import { rejoinLinkStyle, surveyIframeStyle, surveyStyle } from '../styles/Survey.styles';
 
 export interface SurveyProps {
@@ -13,14 +13,20 @@ export interface SurveyProps {
 const SURVEY = 'SurveyComponent';
 
 export const Survey: React.FunctionComponent<SurveyProps> = (props: SurveyProps) => {
+  const surveyType = props.postCall.survey?.type;
+  let postcallSurveyUrl = '';
+  if (surveyType === 'msforms') {
+    const options: MSFormsSurveyOptions = props.postCall.survey?.options as MSFormsSurveyOptions;
+    postcallSurveyUrl = options.surveyUrl;
+  } else if (surveyType === 'custom') {
+    const options: CustomSurveyOptions = props.postCall.survey?.options as CustomSurveyOptions;
+    postcallSurveyUrl = options.surveyUrl;
+  } else {
+    postcallSurveyUrl = '';
+  }
   return (
     <Stack styles={surveyStyle}>
-      <iframe
-        title={SURVEY}
-        style={surveyIframeStyle}
-        src={props.postCall.survey?.options.surveyUrl}
-        scrolling="yes"
-      ></iframe>
+      <iframe title={SURVEY} style={surveyIframeStyle} src={postcallSurveyUrl} scrolling="yes"></iframe>
       <Stack horizontalAlign="center" verticalAlign="center" styles={rejoinLinkStyle}>
         <Link
           onClick={() => {
