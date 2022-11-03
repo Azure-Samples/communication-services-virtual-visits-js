c353d2d748daf9cd5cacef8be266f8699714adf4
Han Kyung Sung <hansung@microsoft.com>
2022-11-02 17:30:51 -0700

    Increasedwait time from 1200ms to 1500ms.

--- e2e-tests/tests/common/utils.ts
+++ e2e-tests/tests/common/utils.ts
@@ -15,7 +15,7 @@ export const buildUrl = (
   }
 };
 
-export const DELAY_MS = 1200;
+export const DELAY_MS = 1500;
 
 const getGUID = () => {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
