3bda92a9c53da7cd7f57e783fe11f29d6ffb5b07
Ralph Mari Gabrinao <gabrinao.ralph@gmail.com>
2022-12-06 11:22:26 -0800

    Remove unused function

--- client/src/styles/JoinTeamsMeeting.Styles.ts
+++ client/src/styles/JoinTeamsMeeting.Styles.ts
@@ -1,16 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export function makeJoinTeamsLayerHostStyles(): any {
-  return {
-    position: 'relative',
-    height: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
-  };
-}
-
 export const formStyles = {
   root: {
     width: 300
