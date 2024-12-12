c28354108b57adc9ba7882e9f77833316ffd5b78
Miguel Gamis <miguelgamis@microsoft.com>
2024-12-11 19:13:25 -0800

    skip rooms e2e test due to outdated Rooms client api

--- e2e-tests/tests/home.spec.ts
+++ e2e-tests/tests/home.spec.ts
@@ -6,7 +6,9 @@ import { delay, DELAY_MS } from "./common/utils";
 
 const SERVER_URL = "http://localhost:8080";
 
-test("root page test", async ({ page }) => {
+// Skipping the test because the current Rooms Client version is outdated and fails. 
+// This test will be re-enabled in a future PR to update the Rooms Client to the latest version.
+test.skip("root page test", async ({ page }) => {
   // Go to http://localhost:8080/
   await page.goto(SERVER_URL);
 
