76e5b4f06953cffa4822ae86a54806605ccf4c27
dmceachernmsft <dmceachern@microsoft.com>
2025-04-23 08:59:46 -0700

    add mock for calling sdk

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -12,3 +12,35 @@ setIconOptions({
 window.AudioContext = jest.fn().mockImplementation(() => {
   return {};
 });
+
+Object.defineProperty(window, 'MediaStreamTrack', {
+  writable: true,
+  value: jest.fn().mockImplementation(() => ({
+    contentHint: 'string',
+    enabled: true,
+    id: 'string',
+    kind: 'string',
+    label: 'string',
+    muted: true,
+    onended: jest.fn(),
+    onmute: jest.fn(),
+    onunmute: jest.fn(),
+    readyState: 'live',
+    applyConstraints: jest.fn(),
+    clone: jest.fn(),
+    getCapabilities: jest.fn(),
+    getConstraints: jest.fn(),
+    getSettings: jest.fn(),
+    stop: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+    isolated: false,
+    onisolationchange: jest.fn()
+  }))
+});
+
+Object.defineProperty(window, 'MediaStream', {
+  writable: true,
+  value: jest.fn().mockImplementation(() => Object.create(MediaStream.prototype))
+});
