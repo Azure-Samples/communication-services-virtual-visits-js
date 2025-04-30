af29a077b4a42c7e454675ad0d575012ad14dca1
dmceachernmsft <dmceachern@microsoft.com>
2025-04-30 10:06:20 -0700

    UI nits

--- client/src/components/rooms/transcription/TranscriptionOptionsModal.tsx
+++ client/src/components/rooms/transcription/TranscriptionOptionsModal.tsx
@@ -41,10 +41,10 @@ export const TranscriptionOptionsModal = (props: TranscriptionModalProps): JSX.E
       styles={{ main: { maxWidth: '27.5rem', height: '16.75rem' } }}
     >
       <Stack tokens={{ childrenGap: '1.5rem' }} styles={{ root: { margin: '1.5rem' } }}>
-        <Stack horizontal verticalAlign="center" style={{ position: 'relative' }}>
-          <Text style={{ fontWeight: 600, fontSize: '1.25rem' }}>What Language is being spoken?</Text>
+        <Stack horizontal verticalAlign="center" style={{ position: 'relative' }} tokens={{ childrenGap: '0.5rem' }}>
+          <Text style={{ fontWeight: 600, fontSize: '1.25rem' }}>What language is being spoken?</Text>
           <IconButton
-            style={{ position: 'absolute', right: 0 }}
+            style={{ right: 0 }}
             onRenderIcon={() => <Dismiss20Regular style={{ color: theme.palette.neutralPrimary }} />}
             onClick={() => {
               setIsOpen(false);
@@ -65,7 +65,7 @@ export const TranscriptionOptionsModal = (props: TranscriptionModalProps): JSX.E
             selectedKey={currentLanguage}
           ></Dropdown>
           <Text style={{ fontSize: '0.75rem', color: theme.palette.neutralSecondary }}>
-            language that everyone on this call is speaking.
+            Language that everyone on this call is speaking.
           </Text>
         </Stack>
         <Stack horizontal tokens={{ childrenGap: '0.5rem' }} horizontalAlign="end" style={{ paddingTop: '1.5rem' }}>
